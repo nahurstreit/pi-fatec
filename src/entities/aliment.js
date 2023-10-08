94c3d456f1e9e5f97cdfe19db227f2e2bf30b538
Samuel Oliveira Azevedo <samueltrader931@gmail.com>
2023-10-08 16:12:33 -0300

    update some requirements

---
+++ src/entities/aliment.js
@@ -0,0 +1,9 @@
+class Aliment{
+    constructor(group, kcal, carb, protein, fat){
+        this.group = group;
+        this.kcal = kcal;
+        this.carb = carb;
+        this.protein = protein;
+        this.fat = fat;
+    }
+}
