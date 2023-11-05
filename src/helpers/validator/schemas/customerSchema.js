4e2d232e4d0ef61080ea8f249500e62a32da46c3
Matheus Aguiar Mendes Valim <131181959+Matheus-Valim@users.noreply.github.com>
2023-11-05 07:50:20 -0300

    Adicionando requisitos do cliente

--- src/helpers/validator/schemas/customerSchema.js
+++ src/helpers/validator/schemas/customerSchema.js
@@ -1,3 +1,49 @@
 import Schema from "validate"
 
-console.log("Matheus")
+const costumer = new Schema ({
+  name: {
+    type: String,
+    required: true,
+    message: "enviar o nome é OBRIGATÓRIO"
+  },
+  email: {
+    type: String,
+    required: true,
+    message: "enviar o email é OBRIGATÓRIO"
+  },
+  password: {
+    type: String,
+    required: true,
+    message: "a senha é OBRIGATÓRIA"
+  },
+  cpf: {
+    type: String,
+    required: true,
+    message: "enviar o CPF é OBRIGATÓRIO"
+  },
+  cellphone: {
+    type: String,
+    required: true,
+    message: "enviar o telefone é OBRIGATÓRIO"
+  },
+  height: {
+    type: String,
+    required: true,
+    message: "enviar a altura é OBRIGATÓRIO"
+  },
+  weight: {
+    type: String,
+    required: true,
+    message: "enviar o peso é OBRIGATÓRIO"
+  },
+  gender: {
+    type: String,
+    required: true,
+    message: "enviar o sexo é OBRIGATÓRIO"
+  },
+  birth: {
+    type: String,
+    required: true,
+    message: "enviar a data de nascimento é OBRIGATÓRIO"
+  }
+})
