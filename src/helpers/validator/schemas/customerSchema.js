de8f111ad9ec9a6cea081682eab2f5cd77cfcd9c
Matheus Aguiar Mendes Valim <131181959+Matheus-Valim@users.noreply.github.com>
2023-11-06 12:45:20 -0300

    Corrigindo indentação

--- src/helpers/validator/schemas/customerSchema.js
+++ src/helpers/validator/schemas/customerSchema.js
@@ -1,51 +1,51 @@
 import Schema from "validate"
 
 const customerSchema = new Schema ({
-  name: {
-    type: String,
-    required: true,
-    message: "enviar o NOME é obrigatório"
-  },
-  email: {
-    type: String,
-    required: true,
-    message: "enviar o EMAIL é obrigatório"
-  },
-  password: {
-    type: String,
-    required: true,
-    message: "enviar a SENHA é obrigatório"
-  },
-  cpf: {
-    type: String,
-    required: true,
-    message: "enviar o CPF é obrigatório"
-  },
-  cellphone: {
-    type: String,
-    required: true,
-    message: "enviar o TELEFONE é obrigatório"
-  },
-  height: {
-    type: String,
-    required: true,
-    message: "enviar a ALTURA é obrigatório"
-  },
-  weight: {
-    type: String,
-    required: true,
-    message: "enviar o PESO é obrigatório"
-  },
-  gender: {
-    type: String,
-    required: true,
-    message: "enviar o SEXO é obrigatório"
-  },
-  birth: {
-    type: String,
-    required: true,
-    message: "enviar a DATA DE NASCIMENTO é obrigatório"
-  }
+    name: {
+        type: String,
+        required: true,
+        message: "enviar o NOME é obrigatório"
+    },
+    email: {
+        type: String,
+        required: true,
+        message: "enviar o EMAIL é obrigatório"
+    },
+    password: {
+        type: String,
+        required: true,
+        message: "enviar a SENHA é obrigatório"
+    },
+    cpf: {
+        type: String,
+        required: true,
+        message: "enviar o CPF é obrigatório"
+    },
+    cellphone: {
+        type: String,
+        required: true,
+        message: "enviar o TELEFONE é obrigatório"
+    },
+    height: {
+        type: String,
+        required: true,
+        message: "enviar a ALTURA é obrigatório"
+    },
+    weight: {
+        type: String,
+        required: true,
+        message: "enviar o PESO é obrigatório"
+    },
+    gender: {
+        type: String,
+        required: true,
+        message: "enviar o SEXO é obrigatório"
+    },
+    birth: {
+        type: String,
+        required: true,
+        message: "enviar a DATA DE NASCIMENTO é obrigatório"
+    }
 })
 
 export default customerSchema
--- src/helpers/validator/schemas/mealSchema.js
+++ src/helpers/validator/schemas/mealSchema.js
@@ -1,20 +1,20 @@
 import Schema from "validate"
 
 const mealSchema = new Schema ({
-  name: {
-    type: String,
-    required: true,
-    message: "enviar o NOME é obrigatório"
-  },
-  hour: {
-    type: String,
-    required: true,
-    message: "enviar a HORA é obrigatório"
-  },
-  obs: {
-    type: String,
-    required: false
-  }
+    name: {
+        type: String,
+        required: true,
+        message: "enviar o NOME é obrigatório"
+    },
+    hour: {
+        type: String,
+        required: true,
+        message: "enviar a HORA é obrigatório"
+    },
+    obs: {
+        type: String,
+        required: false
+    }
 })
 
 export default mealSchema
