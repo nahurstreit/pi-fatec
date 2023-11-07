ea37489c3de236d831546be647305922adc675ce
Samuel Oliveira Azevedo <samueltrader931@gmail.com>
2023-11-06 22:06:13 -0300

    add server config and customers.routes config

--- src/router/customer.routes.js
+++ src/router/customer.routes.js
@@ -3,6 +3,51 @@ import customerModel from "../models/customerModel.js"
 
 
 export default class CustomerRoutes {
+
+    routes() {
+        const router = Router()
+
+        router.route('/')
+        .get((req, res) => res.status(200).json({ path: '/', method: 'get' }))
+        .post((req,res) => res.status(200).json({path: '/', method: 'post'}))
+
+        router.route('/:idCustomer')
+        .post((req,res) => res.status(200).json({path: '/:idCustomer', method: 'post'}))
+        .get((req,res) => res.status(200).json({path: '/:idCustomer', method: 'get'}))
+        .delete((req,res) => res.status(200).json({path: '/:idCustomer', method: 'delete'}))
+        .put((req,res) => res.status(200).json({path: '/:idCustomer', method: 'put'}))
+
+        router.route('/:idCustomer/meal[s]?')
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?', method: 'get'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?', method: 'post'}))
+
+        router.route('/:idCustomer/meal[s]?/:idMeal')
+        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'post'}))
+        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'get'}))
+        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'delete'}))
+        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'put'}))
+        
+        router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?')
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?', method: 'get'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?', method: 'post'}))
+
+        router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood')
+        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'post'}))
+        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'get'}))
+        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'delete'}))
+        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'put'}))
+
+        router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?')
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?', method: 'get'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?', method: 'post'}))
+
+        router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood')
+        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'post'}))
+        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'get'}))
+        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'delete'}))
+        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'put'}))
+
+        return router
+    }
     
 }
-console.log("Samuel")
--- src/server.js
+++ src/server.js
@@ -2,8 +2,15 @@ import { Express } from "express"
 import sequelize from "./database/dbConfig.js"
 import CustomerRoutes from "./router/customer.routes.js"
 
+//console.log("Samuel")
+
 const PORT = 3000 || 3333
 
-console.log("Samuel")
+const app = express()
+app.use(express.json())
 
-const customerRoutes = new CustomerRoutes()
+
+const customerRoutes = new CustomerRoutes();
+app.use('/user[s]?', customerRoutes.routes());
+
+app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) })
