72cae20b13262cadcf148a876f2286eef81c75f1
Samuel Oliveira Azevedo <samueltrader931@gmail.com>
2023-11-07 01:26:48 -0300

    editing some errors

--- src/router/customer.routes.js
+++ src/router/customer.routes.js
@@ -20,30 +20,30 @@ export default class CustomerRoutes {
         .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?', method: 'post'}))
 
         router.route('/:idCustomer/meal[s]?/:idMeal')
-        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'post'}))
-        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'get'}))
-        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'delete'}))
-        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'put'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'post'}))
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'get'}))
+        .delete((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'delete'}))
+        .put((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal', method: 'put'}))
         
         router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?')
         .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?', method: 'get'}))
         .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?', method: 'post'}))
 
         router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood')
-        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'post'}))
-        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'get'}))
-        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'delete'}))
-        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'put'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'post'}))
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'get'}))
+        .delete((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'delete'}))
+        .put((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood', method: 'put'}))
 
         router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?')
         .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?', method: 'get'}))
         .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?', method: 'post'}))
 
         router.route('/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood')
-        .post((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'post'}))
-        .get((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'get'}))
-        .delete((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'delete'}))
-        .put((res,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'put'}))
+        .post((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'post'}))
+        .get((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'get'}))
+        .delete((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'delete'}))
+        .put((req,res) => res.status(200).json({path: '/:idCustomer/meal[s]?/:idMeal/food[s]?/:idFood/subfood[s]?/:idSubFood', method: 'put'}))
 
         return router
     }
