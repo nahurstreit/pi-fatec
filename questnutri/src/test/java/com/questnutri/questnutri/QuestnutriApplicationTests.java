f484facdecf27b66fa5cfc566805070acadfc1cd
Samuel Azevedo <samueltrader931@gmail.com>
2024-05-04 13:40:18 -0300

    add SpringBoot directory management + models/controllers directories

---
+++ questnutri/src/test/java/com/questnutri/questnutri/QuestnutriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questnutri.questnutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestnutriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
