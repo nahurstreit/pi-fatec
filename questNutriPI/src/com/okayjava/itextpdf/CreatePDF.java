1ca096a8973432e3ee1a4f3123894e1aedb12821
Bruno Soares <brunosoaresdasilva25@gmail.com>
2024-05-23 17:14:38 -0300

    Update class CreatePDF
    Inserting the print function

--- questNutriPI/src/com/okayjava/itextpdf/CreatePDF.java
+++ questNutriPI/src/com/okayjava/itextpdf/CreatePDF.java
@@ -5,7 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Locale;
+
+import javax.swing.JOptionPane;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
@@ -21,145 +22,145 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import model.entities.Customer;
+
 public class CreatePDF {
 
-    // Nome do arquivo PDF a ser criado
-    private static final String PDF_NAME = "QuestNutri.pdf";
+	private static final String PDF_NAME = "QuestNutri.pdf";
+	private static final String IMG_PATH = "../img/QuestNutri.png";
 
-    // Caminho relativo para a imagem a partir do diretório 'questNutriPI/src'
-    private static final String IMG_NAME = "../img/QuestNutri.png";
+	public static void main(String[] args) {
+		System.out.println("Criar PDF usando iText");
 
-    /**
-     * Método principal para a criação do PDF.
-     *
-     * @param args argumentos da linha de comando (não utilizado)
-     */
-    public static void main(String[] args) {
-        System.out.println("Criar PDF usando iText");
+		// Caminho para a pasta "Downloads" do usuário atual
+		String userHome = System.getProperty("user.home");
+		String downloadPath = Paths.get(userHome, "Downloads", PDF_NAME).toString();
 
-        // Determinar o diretório de downloads do usuário
-        Path downloadPath = getDownloadDirectory();
-        if (downloadPath == null) {
-            System.err.println("Não foi possível determinar o diretório de downloads.");
-            return;
-        }
+		// 1. Documento
+		Document document = new Document();
 
-        // 1. Criar um objeto de Documento
-        Document document = new Document();
+		// Escritor de PDF
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(new File(downloadPath)));
 
-        // Criar um escritor de PDF
-        try {
-            File pdfFile = new File(downloadPath.toFile(), PDF_NAME);
-            PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+			// Abrir documento
+			document.open();
 
-            // Abrir o Documento
-            document.open();
+			// Adicionar Parágrafo
+			String space1 = "\n\n\n\n";
 
-            // 3. Criar um chunk (um pedaço de texto)
-            Font font = FontFactory.getFont(FontFactory.COURIER_BOLD, 14, BaseColor.BLUE);
-            Chunk chunk = new Chunk("QuestNutri", font);
+			// Criar objeto de parágrafo
+			Paragraph paragraph = new Paragraph(space1);
 
-            // Adicionar texto ao PDF
-            document.add(chunk);
+			// Adicionar parágrafo ao PDF
+			document.add(paragraph);
 
-            // Adicionar um Parágrafo
-            String multiLineText = "Consultas";
+			// 3. Criar um chunk
+			Font font = FontFactory.getFont(FontFactory.COURIER_BOLD, 14, BaseColor.BLUE);
+			Chunk chunk = new Chunk("Nome da Nutricionista - Dieta do cliente: " + Customer.findByPK(1).name, font);
 
-            // Criar um objeto de parágrafo
-            Paragraph paragraph = new Paragraph(multiLineText);
+			// Adicionar texto ao pdf
+			document.add(chunk);
 
-            // Adicionar o parágrafo ao PDF
-            document.add(paragraph);
+			// Adicionar Parágrafo
+			String multiLineText = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
-            // Adicionar uma imagem ao PDF
-            Path imgPath = Paths.get(IMG_NAME); // Usar o caminho relativo
+			// Criar objeto de parágrafo
+			Paragraph paragraph1 = new Paragraph(multiLineText);
 
-            // Criar um objeto de imagem
-            Image image = Image.getInstance(imgPath.toAbsolutePath().toString());
-            image.scaleAbsolute(500f, 500f);
+			// Adicionar parágrafo ao PDF
+			document.add(paragraph1);
 
-            // Adicionar a imagem ao PDF
-            document.add(image);
+			// Adicionar imagem ao pdf
+			Path imgPath = Paths.get(IMG_PATH);
 
-            // Adicionar atributos - título, data de criação, autor
-            document.addAuthor("QUESTNUTRI");
-            document.addCreationDate();
-            document.addProducer();
-            document.addCreator("Teste do PI");
-            document.addTitle("Dieta");
-            document.setPageSize(PageSize.LETTER);
-            document.addSubject("QuestNutri - pdf");
-            document.addKeywords("QuestNutri, PI, Dieta");
+			// Criar objeto de Imagem
+			Image image = Image.getInstance(imgPath.toAbsolutePath().toString());
+			image.scaleAbsolute(100f, 100f);
 
-            // Inserir uma Tabela
-            float[] columns = { 150F, 150F, 150F };
-            PdfPTable table = new PdfPTable(columns);
+			// Centralizar a imagem
+			float x = (document.getPageSize().getWidth() - image.getScaledWidth()) / 2;
+			float y = (document.getPageSize().getHeight() - image.getScaledHeight()) - 10;
 
-            // Criar uma célula
-            PdfPCell cell1 = new PdfPCell();
-            cell1.setPhrase(new Phrase("Primeiro Nome"));
-            table.addCell(cell1);
+			image.setAbsolutePosition(x, y);
 
-            // Criar a 2ª Célula
-            PdfPCell cell2 = new PdfPCell();
-            cell2.setPhrase(new Phrase("Último Nome"));
-            table.addCell(cell2);
+			// Adicionar imagem ao pdf
+			document.add(image);
 
-            // Criar a 3ª Célula
-            PdfPCell cell3 = new PdfPCell();
-            cell3.setPhrase(new Phrase("Email"));
-            table.addCell(cell3);
+			// Adicionar atributos - título, data de criação, autor
+			document.addAuthor("QUESTNUTRI");
+			document.addCreationDate();
+			document.addProducer();
+			document.addCreator("Teste do PI");
+			document.addTitle("Dieta");
+			document.setPageSize(PageSize.LETTER);
+			document.addSubject("QuestNutri - pdf");
+			document.addKeywords("QuestNutri, PI, Dieta");
 
-            // Valores de Dados
-            // Criar a 4ª célula
-            PdfPCell cell4 = new PdfPCell();
-            cell4.setPhrase(new Phrase("Bruno"));
-            table.addCell(cell4);
+			// Inserir uma Tabela
+			float[] columns = { 50F, 50F, 50F, 50F, 50F, 50F, 50F };
+			PdfPTable table = new PdfPTable(columns);
 
-            // Criar a 5ª Célula
-            PdfPCell cell5 = new PdfPCell();
-            cell5.setPhrase(new Phrase("Soares"));
-            table.addCell(cell5);
+			// Criar uma célula
+			PdfPCell cell1 = new PdfPCell();
+			cell1.setPhrase(new Phrase("Domingo"));
+			table.addCell(cell1);
 
-            // Criar a 6ª Célula
-            PdfPCell cell6 = new PdfPCell();
-            cell6.setPhrase(new Phrase("bruno@gmail.com"));
-            table.addCell(cell6);
+			// Criar 2ª célula
+			PdfPCell cell2 = new PdfPCell();
+			cell2.setPhrase(new Phrase("Segunda-feira"));
+			table.addCell(cell2);
 
-            // Adicionar a tabela ao documento
-            document.add(table);
+			// Criar 3ª célula
+			PdfPCell cell3 = new PdfPCell();
+			cell3.setPhrase(new Phrase("Terça-feira"));
+			table.addCell(cell3);
 
-            // Fechar o documento
-            document.close();
+			// Valores de Dados
+			// Criar 4ª célula
+			PdfPCell cell4 = new PdfPCell();
+			cell4.setPhrase(new Phrase("Quarta-feira"));
+			table.addCell(cell4);
 
-            System.out.println("PDF criado e salvo em " + pdfFile.getAbsolutePath());
+			// Criar 5ª célula
+			PdfPCell cell5 = new PdfPCell();
+			cell5.setPhrase(new Phrase("Quinta-feira"));
+			table.addCell(cell5);
 
-        } catch (IOException | DocumentException e) {
-            e.printStackTrace();
-        }
-    }
+			// Criar 6ª célula
+			PdfPCell cell6 = new PdfPCell();
+			cell6.setPhrase(new Phrase("Sexta-feira"));
+			table.addCell(cell6);
 
-    /**
-     * Obtém o diretório de downloads do usuário de forma multiplataforma.
-     *
-     * @return o Path para o diretório de downloads do usuário, ou null se não puder ser determinado.
-     */
-    private static Path getDownloadDirectory() {
-        String userHome = System.getProperty("user.home");
-        if (userHome == null) {
-            return null;
-        }
+			// Criar 7ª célula
+			PdfPCell cell7 = new PdfPCell();
+			cell7.setPhrase(new Phrase("Sábado"));
+			table.addCell(cell7);
 
-        // Determina o diretório de downloads com base no sistema operacional
-        String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
-        if (os.contains("win")) {
-            return Paths.get(userHome, "Downloads");
-        } else if (os.contains("mac")) {
-            return Paths.get(userHome, "Downloads");
-        } else if (os.contains("nix") || os.contains("nux")) {
-            return Paths.get(userHome, "Downloads");
-        }
+			// Adicionar tabela ao documento
+			document.add(table);
 
-        return null;
-    }
+			// Fechar o documento
+			document.close();
+
+			// Perguntar ao usuário se deseja imprimir o PDF
+			int response = JOptionPane.showConfirmDialog(null, "Deseja imprimir o PDF?", "Imprimir PDF",
+					JOptionPane.YES_NO_OPTION);
+			if (response == JOptionPane.YES_OPTION) {
+				// Perguntar ao usuário se a impressão será colorida ou em preto e branco
+				Object[] options = { "Colorido", "Preto e Branco" };
+				int colorResponse = JOptionPane.showOptionDialog(null, "Escolha o modo de impressão:",
+						"Modo de Impressão", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options,
+						options[0]);
+
+				// Imprimir o PDF
+				PrinterPDF.printPDF(downloadPath, colorResponse == 0);
+			}
+
+			System.out.println("PDF criado.");
+
+		} catch (IOException | DocumentException e) {
+			e.printStackTrace();
+		}
+	}
 }
