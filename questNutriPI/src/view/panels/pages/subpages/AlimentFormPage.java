3dd16671959377b7cbd39c4dc829c822acf0ba65
Bruno Soares <brunosoaresdasilva25@gmail.com>
2024-06-11 22:31:51 -0300

    Update class AlimentFormPage
    Adding validations to the name, in the ash and in humidity.

--- questNutriPI/src/view/panels/pages/subpages/AlimentFormPage.java
+++ questNutriPI/src/view/panels/pages/subpages/AlimentFormPage.java
@@ -1,5 +1,6 @@
 package view.panels.pages.subpages;
 
+import jakarta.persistence.Column;
 import model.entities.Aliment;
 import utils.FoodUtil;
 import validate.Validate;
@@ -29,6 +30,9 @@ public class AlimentFormPage extends GenericFormPage {
 		private FormBoxInput humidity;
 		private FormBoxInput ash;
 	
+	// Valores padrões de validação
+		private final int MIN_SIZE_NAME = 5;
+		private final int MAX_SIZE_NAME = 50;
 
 	public AlimentFormPage(GenericJPanel ownerPanel, Aliment aliment) {
 		super(ownerPanel);
@@ -45,7 +49,7 @@ public class AlimentFormPage extends GenericFormPage {
 					} catch (Exception e) {
 						return false;
 					}
-					}, new LanguageUtil("Inválido", "Invalid").get());
+					}, new LanguageUtil("Não é permitido ter letras!", "Letters are not allowed!").get());
 		
 	}
 	
@@ -73,7 +77,17 @@ public class AlimentFormPage extends GenericFormPage {
 	protected FormSection basicInfo() {
 		//Essenciais
 			name = new FormBoxInput(this).setLbl(new LanguageUtil("Nome", "Name").get())
-										 .setValue(aliment.name);
+										 .setValue(aliment.name)
+										 .addValidation(
+												 new ValidationRule(
+														 value -> {
+															 return !Validate.hasNumber(value);
+														 }, new LanguageUtil("Não é permitido ter números!", "Numbers are not allowed!").get()),
+												 new ValidationRule(
+														 value -> {
+															 return Validate.sizeBetween(value, MIN_SIZE_NAME, MAX_SIZE_NAME);
+														 }, new LanguageUtil("Tamanho precisa ser entre "+MIN_SIZE_NAME+" e "+MAX_SIZE_NAME+".", "Size must be between "+MIN_SIZE_NAME+" and "+MAX_SIZE_NAME+".").get())
+												 );
 			
 			group = new FormBoxInput(this).setLbl(new LanguageUtil("Grupo Alimentar", "Group").get())
 										  .setValue(aliment.alimentGroup)
@@ -88,17 +102,84 @@ public class AlimentFormPage extends GenericFormPage {
 					 					 .addValidation(isDouble());
 			
 			protein = new FormBoxInput(this).setLbl(new LanguageUtil("Proteína", "Protein").get())
-					 						.setValue(aliment.protein);
+					 						.setValue(aliment.protein)
+					 						.addValidation(isDouble());
 			
 			fat = new FormBoxInput(this).setLbl(new LanguageUtil("Gordura", "Fat").get())
-					 					.setValue(aliment.fat);
+					 					.setValue(aliment.fat)
+					 					.addValidation(isDouble());
 			
 		//Secundários
 			humidity = new FormBoxInput(this).setLbl(new LanguageUtil("Umidade (%)", "Humidity (%)").get())
-											 .setValue(aliment.humidity);
+											 .setValue(aliment.humidity)
+											 .addValidation(
+						 					 			new ValidationRule(
+						 					 					value -> {
+						 					 						return !Validate.hasChar(value, ',', '.', '%');
+					                                              }, new LanguageUtil("Não é permitido ter letras!", "Letters are not allowed!").get())
+						 					 			);
+//			@Column(name = "dietaryFiber")
+//			public String dietaryFiber;
+//
+//			@Column(name = "cholesterol")
+//			public String cholesterol;
+//
+//			@Column(name = "sodium")
+//			public String sodium;
+//
+//			@Column(name = "calcium")
+//			public String calcium;
+//
+//			@Column(name = "magnesium")
+//			public String magnesium;
+//
+//			@Column(name = "manganese")
+//			public String manganese;
+//
+//			@Column(name = "phosphorus")
+//			public String phosphorus;
+//
+//			@Column(name = "iron")
+//			public String iron;
+//
+//			@Column(name = "potassium")
+//			public String potassium;
+//
+//			@Column(name = "copper")
+//			public String copper;
+//
+//			@Column(name = "zinc")
+//			public String zinc;
+//
+//			@Column(name = "retinol")
+//			public String retinol;
+//
+//			@Column(name = "RE")
+//			public String rE;
+//
+//			@Column(name = "RAE")
+//			public String rAE;
+//
+//			@Column(name = "thiamine")
+//			public String thiamine;
+//
+//			@Column(name = "riboflavin")
+//			public String riboflavin;
+//
+//			@Column(name = "pyridoxine")
+//			public String pyridoxine;
+//
+//			@Column(name = "niacin")
+//			public String niacin;
+//
+//			@Column(name = "vitaminC")
+//			public String vitaminC;
 			
 			ash = new FormBoxInput(this).setLbl(new LanguageUtil("Cinzas", "Ash").get())
-										.setValue(aliment.ash);
+										.setValue(aliment.ash)
+										.addValidation(isDouble());
+			
+			
 		
 		FormSection basicInfo = new FormSection(this).setUpName(new LanguageUtil("Informações Básicas", "Basic Information").get());
 		
@@ -130,7 +211,7 @@ public class AlimentFormPage extends GenericFormPage {
 		
 		basicInfo.addRow(name, group, kcalBox)
 				 .addRow(carb, protein, fat)
-				 .addRow(ash);
+				 .addRow(humidity, ash);
 		
 		
 		return basicInfo;
