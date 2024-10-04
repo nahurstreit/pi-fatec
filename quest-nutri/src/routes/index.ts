f26625bba86696bf8b47f48ec9e279914e298fe4
Lucas P Souza <107370459+LucasP-Souza@users.noreply.github.com>
2024-10-04 09:56:25 -0300

    Model, Controller, Service and Repository created for the nutritionist

--- quest-nutri/src/routes/index.ts
+++ quest-nutri/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import patientRoutes from './patients'
+import nutritionistRoutes from './nutritionists'
 
 const routes = Router()
 routes.use('/patient', patientRoutes)
+routes.use('/nutritionist', nutritionistRoutes)
 
 //routes.use('/docs', swagger)
 
