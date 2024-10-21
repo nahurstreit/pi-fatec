4edb24e89df92bc12ffa62e534b856d9f1e3dbaf
Lucas P Souza <107370459+LucasP-Souza@users.noreply.github.com>
2024-10-21 11:06:57 -0300

    Starting swagger creation

--- quest-nutri/src/routes/index.ts
+++ quest-nutri/src/routes/index.ts
@@ -4,12 +4,14 @@ import nutritionistRoutes from './nutritionists'
 import adminRoutes from './admin'
 import authRoutes from './auth'
 import { authAdmin, authNutritionist, authPatient } from '../middlewares/auth/auth.middleware'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './../swagger.json'
 
 const routes = Router()
 routes.use('/auth', authRoutes)
 routes.use('/patient', authPatient, patientRoutes)
 routes.use('/nutritionist', authNutritionist, nutritionistRoutes)
 routes.use('/admin', authAdmin, adminRoutes)
-//routes.use('/docs', swagger)
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export default routes
